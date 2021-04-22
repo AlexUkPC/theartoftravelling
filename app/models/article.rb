92b9d64aeff65c7c26633b27cae623d22d5b84d0
AlexUkPC <alexrogna@yahoo.com>
2021-04-22 19:26:43 +0100

    broadcast new article with action cable

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,7 @@ class Article < ApplicationRecord
   has_one_attached :cover_image
   attr_accessor :remove_cover_image
   after_save { cover_image.purge if remove_cover_image == '1' }
+  after_save :broadcast_new_article
   has_rich_text :body
   scope :published, -> { where.not(published_at: nil) }
   scope :draft, -> { where(published_at: nil) }
@@ -22,5 +23,17 @@ class Article < ApplicationRecord
   def owned_by?(owner)
     return false unless owner.is_a?(User)
     user == owner
+  end
+  private
+  def broadcast_new_article
+    if published? && saved_change_to_published_at?
+      ActionCable.server.broadcast(
+        "articles:new",
+        new_article: ArticlesController.render(
+          partial: 'articles/new_article_notification',
+          locals: { article: self }
+        )
+      )
+    end
   end
 end

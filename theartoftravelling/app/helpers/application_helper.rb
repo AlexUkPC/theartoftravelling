70ade0f9e91a0a014b81b47e34a08f622a177faa
AlexUkPC <alexrogna@yahoo.com>
2021-07-13 09:07:41 +0100

    add date and changes in index layout

--- theartoftravelling/app/helpers/application_helper.rb
+++ theartoftravelling/app/helpers/application_helper.rb
@@ -9,4 +9,7 @@ module ApplicationHelper
   def submit_or_cancel(form, name = "Cancel")
     form.submit + " or " + link_to(name, 'javascript:history.go(-1);', class: 'cancel')
   end
+  def verbose_date(date)
+    date.strftime('%B %d %Y')
+  end
 end
